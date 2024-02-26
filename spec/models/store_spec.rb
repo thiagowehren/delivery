46c8e3d35655db10806eb76901a23f8ad00ca88a
thiagowehren <thiago-ws@hotmail.com>
2024-02-26 16:38:40 -0300

    add store model valisations: name presence and length

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Store, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe "validations" do
+    it "should be valid with valid attributes" do
+      store = Store.new(name: "templateTest Store")
+      expect(store).to be_valid
+    end
+
+    it "should not be valid without a name" do
+      store = Store.new(name: nil)
+      expect(store).not_to be_valid
+      expect(store.errors[:name]).to include("can't be blank")
+    end
+
+    it "is not valid with a name shorter than 3 characters" do
+      store = Store.new(name: "te")
+      expect(store).not_to be_valid
+      expect(store.errors[:name]).to include("is too short (minimum is 3 characters)")
+    end
+  end
 end
