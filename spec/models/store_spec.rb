1cf7850026652101d583c5abe0e044b5400b05f0
thiagowehren <thiago-ws@hotmail.com>
2024-02-27 16:57:50 -0300

    refactor store model tests to use shoulda-matchers

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -3,21 +3,7 @@ require 'rails_helper'
 RSpec.describe Store, type: :model do
   
   describe "validations" do
-    it "should be valid with valid attributes" do
-      store = Store.new(name: "templateTest Store")
-      expect(store).to be_valid
-    end
-
-    it "should not be valid without a name" do
-      store = Store.new(name: nil)
-      expect(store).not_to be_valid
-      expect(store.errors[:name]).to include("can't be blank")
-    end
-
-    it "is not valid with a name shorter than 3 characters" do
-      store = Store.new(name: "te")
-      expect(store).not_to be_valid
-      expect(store.errors[:name]).to include("is too short (minimum is 3 characters)")
-    end
+    it { should validate_presence_of(:name) }
+    it { should validate_length_of(:name).is_at_least(3) }
   end
 end
