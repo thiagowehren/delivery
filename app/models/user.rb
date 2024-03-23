5f9b43044a342691bf2300f7388986a27fbc9f18
thiagowehren <thiago-ws@hotmail.com>
2024-03-23 01:25:40 -0300

    modify user model to include role attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  enum :role, [:admin, :buyer, :seller]
   has_many :stores
   
   devise :database_authenticatable, :registerable,
