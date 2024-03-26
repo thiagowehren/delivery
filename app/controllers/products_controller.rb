aeec8aefa07e9746fdaf52a658773983a921beab
thiagowehren <thiago-ws@hotmail.com>
2024-03-25 22:07:43 -0300

    add basic sign out/login/logout screen and protect /listing route for admins

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
+    before_action :authenticate_user!
 
     def listing
+        if !current_user.admin?
+            flash[:notice] = "Denied Access!"
+            redirect_to root_path #, notice: "Denied Access!"
+        end
+        
         @products = Product.includes(:store)
     end
 end
