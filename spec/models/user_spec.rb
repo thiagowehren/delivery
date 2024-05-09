506523867acd48e1ebef094c5e5dc56aba87e486
thiagowehren <thiago-ws@hotmail.com>
2024-05-09 14:57:10 -0300

    add additional tests for User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do  
+  describe "validations" do
+    it { should validate_presence_of(:role) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should allow_value("user@example.com").for(:email) }
+    it { should_not allow_value("invalid_email").for(:email) }
+  end
+
+  describe "associations" do
+    it { should have_many(:stores) }
+    it { should have_many(:orders) }
+  end
 end
