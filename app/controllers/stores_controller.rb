c7f6e2b339da109e39cfb81baa9edbfd2e89a3c3
thiagowehren <thiago-ws@hotmail.com>
2024-05-14 04:10:18 -0300

    add restrictions to store editing and deletion, allow admin to create stores for sellers
    - non-owners (buyers and sellers) of a Store are restricted from editing and deleting a Store.
    - admins are allowed to create stores for sellers.
    - users can view all stores.

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,11 +1,12 @@
 class StoresController < ApplicationController
+  skip_forgery_protection only: %i[create update]
   before_action :authenticate!
   before_action :set_store, only: %i[ show edit update destroy ]
 
   # GET /stores or /stores.json
   def index
     user = current_user()
-    if user.admin?
+    if user.admin? || user.buyer?
       @stores = Store.all 
     else
       @stores = Store.where(user: user[:id])
@@ -19,10 +20,14 @@ class StoresController < ApplicationController
   # GET /stores/new
   def new
     @store = Store.new
+    if current_user.admin?
+      @sellers = User.where(role: :seller)
+    end
   end
 
   # GET /stores/1/edit
   def edit
+    return if redirected_cause_user_not_owner_and_not_admin?
   end
 
   # POST /stores or /stores.json
@@ -45,6 +50,8 @@ class StoresController < ApplicationController
 
   # PATCH/PUT /stores/1 or /stores/1.json
   def update
+    return if redirected_cause_user_not_owner_and_not_admin?
+    
     respond_to do |format|
       if @store.update(store_params)
         format.html { redirect_to store_url(@store), notice: "Store was successfully updated." }
@@ -58,6 +65,8 @@ class StoresController < ApplicationController
 
   # DELETE /stores/1 or /stores/1.json
   def destroy
+    return if redirected_cause_user_not_owner_and_not_admin?
+
     @store.destroy!
 
     respond_to do |format|
@@ -80,5 +89,21 @@ class StoresController < ApplicationController
       else
         required.permit(:name)
       end
+    end
+
+    #pair with early return "return if true" to prevent DoubleRenderError
+    def redirected_cause_user_not_owner_and_not_admin?
+      redirected_bool = false
+
+      if current_user != @store.user && !current_user.admin?
+        redirected_bool = true
+
+        respond_to do |format|
+          format.html { redirect_to stores_url, alert: "User doesn't match with store Owner" }
+          format.json { render json: {error: "Unauthorized"}, status: :unauthorized  }
+        end
+      end
+      
+      redirected_bool
     end
 end
