24eac50edd5f8b3e5abfed06a243212ba2f5a70d
thiagowehren <thiago-ws@hotmail.com>
2024-05-17 01:48:50 -0300

    allow creation of orders only with valid order_items

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,16 +4,39 @@ class OrdersController < ApplicationController
     before_action :only_buyers!
 
     def index
-        @orders = Order.where(buyer: current_user)
+        @orders = Order.where(buyer: current_user).order(created_at: :desc)
         render'orders/index', status: :ok
     end
 
     def create
-        @order = Order.new(order_params)
-        # @order = Order.new(order_params) { |o| o.buyer = current_user}
-        @order.buyer = current_user
+        if order_params[:store_id].blank? || order_params[:order_items].blank? || order_params[:order_items].empty?
+            render json: { status: 400, error: "Bad Request" }, status: :bad_request
+            return
+        end
+
+        @order = Order.new(store_id: order_params[:store_id], buyer: current_user)
+
+        order_item_list = []
+        order_items_params = order_params[:order_items]
+        
+        order_items_params.each do |order_item|
+            order_item = OrderItem.new(order_item)
+            order_item.order = @order
+            order_item_list << order_item
+
+            if !order_item.valid?
+                @order.errors.add(:base, "Failed to add one of the items to the order. [item_id: #{order_item.product_id}]")
+                @order.errors.add(:causes, "#{order_item.errors.full_messages}")
+                
+                # p order_item.errors.full_messages
+                render json: { errors: @order.errors }, status: :unprocessable_entity
+                return
+            end
+        end
 
         if @order.save
+            order_item_list.map(&:save)
+
             render template: 'orders/create', status: :created
         else
             render json: {errors: @order.errors}, status: :unprocessable_entity
@@ -29,6 +52,9 @@ class OrdersController < ApplicationController
     private
 
     def order_params
-        params.require(:order).permit([:store_id])
+        params.require(:order).permit(
+            :store_id,
+            order_items: [:product_id, :amount]
+        )
     end
 end
--- app/views/orders/create.json.jbuilder
+++ app/views/orders/create.json.jbuilder
@@ -1,3 +1,11 @@
-json.order do 
-    json.id @order.id
+json.order do
+	json.id @order.id
+	json.order_items @order.order_items do |item|
+		json.product do
+		json.product_id item.product.id
+		json.amount item.amount
+		json.product_price item.product.price
+		json.total_price item.price
+		end
+	end
 end
