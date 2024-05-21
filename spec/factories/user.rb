dca982f18d64b4b9d38a3fdc91c64923198bea91
thiagowehren <thiago-ws@hotmail.com>
2024-05-21 03:08:06 -0300

    add admin user type to User factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -14,4 +14,12 @@ FactoryBot.define do
     password_confirmation { "123456" }
     role { :buyer }
   end
+
+  factory :admin_user, class: User do
+    # Devise
+    sequence(:email) { |n| "factorybotadminser#{n}@example.com"}
+    password { "123456" }
+    password_confirmation { "123456" }
+    role { :admin }
+  end
 end
