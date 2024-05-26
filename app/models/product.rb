ac45f9da7df40a2660dc9db38d32f475764a70f5
thiagowehren <thiago-ws@hotmail.com>
2024-05-26 02:11:12 -0300

    add title validation length to Product Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   acts_as_paranoid
   
   belongs_to :store
+  validates :title, presence: true, length: {minimum: 1}
 
   has_many :orders, through: :order_items
 end
