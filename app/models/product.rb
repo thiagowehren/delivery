7f1f5fe8138e8250c766865fbcb978b3a89039fc
thiagowehren <thiago-ws@hotmail.com>
2024-05-27 22:21:30 -0300

    add image attachment to Product and Store model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,16 @@ class Product < ApplicationRecord
   acts_as_paranoid
   
   belongs_to :store
+  has_one_attached :image
   validates :title, presence: true, length: {minimum: 1}
 
   has_many :orders, through: :order_items
+
+  def thumbnail
+    image.variant(resize_to_limit: [100, 100]).processed { |v| v.quality(50) }
+  end
+
+  def medium
+    image.variant(resize_to_limit: [200, 200]).processed { |v| v.quality(70) }
+  end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -4,11 +4,21 @@ class Store < ApplicationRecord
     belongs_to :user
     before_validation :ensure_seller
     has_many :products, dependent: :destroy
+    has_one_attached :image
     validates :name, presence: true, length: {minimum: 3}
+    
+    def thumbnail
+        image.variant(resize_to_limit: [100, 100]).processed { |v| v.quality(50) }
+    end
+    
+    def medium
+        image.variant(resize_to_limit: [200, 200]).processed { |v| v.quality(70) }
+    end
 
     private
 
     def ensure_seller
         self.user = nil if self.user && !self.user.seller?
     end
+
 end
