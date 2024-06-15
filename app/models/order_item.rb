1ec02dfa53b6f6214fe00ad772acd2bb68ea2d6e
thiagowehren <thiago-ws@hotmail.com>
2024-06-14 22:54:36 -0300

    ensure soft deleted products are displayed in orders
    fix issue of trying to access soft deleted products belonging to orders, preventing subsequent field access errors

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,6 @@
 class OrderItem < ApplicationRecord
     belongs_to :order
-    belongs_to :product
+    belongs_to :product, -> { with_deleted }
     validate :store_product
     
     validates :amount, numericality: { greater_than: 0 }
